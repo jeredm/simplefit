5e146f36bf5716e6028ff7a34b5496e45076be65
Jered Myers <jeredamyers@gmail.com>
2016-12-23 14:43:11 -0800

    Add stubs for Admin Removed files that were not being used.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Menu } from 'semantic-ui-react'
 import { HomeView } from './views/Home/HomeView'
 import { FitRecordView } from './views/FitRecord/FitRecordView'
 import { TeamView } from './views/Team/TeamView'
+import { AdminView } from './views/Admin/AdminView'
 import Footer from './components/Footer'
 
 const menuItems = [
   { name: 'Home' },
   { name: 'Fit Record' },
   { name: 'Team'},
+  { name: 'Admin'},
 ]
 
 class App extends Component {
@@ -26,6 +28,7 @@ class App extends Component {
     if (activeItem === 'Home') return <HomeView />
     if (activeItem === 'Fit Record') return <FitRecordView />
     if (activeItem === 'Team') return <TeamView />
+    if (activeItem === 'Admin') return <AdminView />
 
     return <div>Unknown activeItem: {activeItem}</div>
   }
---
+++ src/views/Admin/AdminView.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class AdminView extends Component {
+	render() {
+		return (
+			<div>
+				<p>Here the admin view where you setup teams and team manager accounts.</p>
+			</div>
+		)
+	}
+}
