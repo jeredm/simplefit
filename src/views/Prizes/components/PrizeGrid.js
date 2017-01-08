01c1c88063c173ce3e699eaedc6f842e186822a7
Jered Myers <jeredamyers@gmail.com>
2017-01-07 16:51:40 -0800

    A winner is chosen

--- src/views/Prizes/components/PrizeGrid.js
+++ src/views/Prizes/components/PrizeGrid.js
@@ -5,10 +5,15 @@ import _ from 'lodash'
 class PrizeGrid extends Component {
   constructor(props) {
     super(props)
+    const cards = this.parseExercises(this.props.users)
     this.state = {
-      cards: this.parseExercises(this.props.users),
+      cards,
       enterLeaveAnimation: 'accordianVertical',
       shuffle: 0,
+      winner: '',
+      remaining: this.props.users,
+      maxShuffle: 20,
+      shuffling: false,
     }
   }
 
@@ -29,6 +34,23 @@ class PrizeGrid extends Component {
     return userCards
   }
 
+  chooseWinner = () => {
+    let winners = []
+    const remaining = this.state.remaining
+
+    _.each(remaining, user => {
+      for (let i = 0; i < user.exercises; i++) {
+        winners.push(user.name)
+      }
+    })
+
+    for (let i = 0; i < 50; i ++) {
+      winners = _.shuffle(winners)
+    }
+
+    return winners[0]
+  }
+
   buildIcon = (excercises) => {
     return (
       <Label size='large'>
@@ -39,11 +61,27 @@ class PrizeGrid extends Component {
 
   shuffle = () => {
     const shuffleCount = this.state.shuffle + 1
-    if (shuffleCount < 20) {
+    if (shuffleCount < this.state.maxShuffle) {
+      let winner = this.state.winner
+      if (shuffleCount === 1) {
+        winner = this.chooseWinner()
+      }
       window.setTimeout(() => { this.shuffle() }, 600)
-      this.setState({ cards: _.shuffle(this.state.cards), shuffle: shuffleCount })
+      this.setState({
+        cards: _.shuffle(this.state.cards),
+        shuffle: shuffleCount,
+        winner,
+        shuffling: true,
+      })
     } else {
-      this.setState({ shuffle: 0 })
+      this.setState({ shuffle: 0, shuffling: false })
+    }
+  }
+
+  buildWinnerLabel = () => {
+    const { shuffle, winner } = this.state
+    if (shuffle === 0 && winner !== '') {
+      return <h1>Winner: {this.state.winner}</h1>
     }
   }
 
@@ -51,7 +89,15 @@ class PrizeGrid extends Component {
     return (
       <div>
         <div>
-          <Button positive size='massive' onClick={this.shuffle} >Choose a Winner</Button>
+          <Button
+            positive
+            size='massive'
+            onClick={this.shuffle}
+            disabled={this.state.shuffling}
+          >
+            Choose a Winner
+          </Button>
+          {this.buildWinnerLabel()}
         </div>
         <div>
           &nbsp;
